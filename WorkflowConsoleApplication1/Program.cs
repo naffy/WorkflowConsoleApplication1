780b3b0d6dcbdd481fce8478e63104498c9d8576
naffy <nawaf.baraya@wolterskluwer.co.uk>
2016-10-14 14:02:56 +0100

    Add project files.

---
+++ WorkflowConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+using System.Xml;
+using System.IO;
+using System.Xaml;
+using System.Activities.XamlIntegration;
+using System.Text;
+
+namespace WorkflowConsoleApplication1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XamlSchemaContext schemaContext = new XamlSchemaContext();
+
+            object xamlLoadedObject;
+            using (var xamlReader = new XamlXmlReader("Workflow1.xaml"))
+            {
+                var builderReader = ActivityXamlServices.CreateBuilderReader(xamlReader, schemaContext);
+                xamlLoadedObject = XamlServices.Load(builderReader);
+                builderReader.Close();
+            }
+            ActivityBuilder activityBuilder = (ActivityBuilder)xamlLoadedObject;
+
+
+            XmlWriterSettings writerSettings = new XmlWriterSettings { Indent = true };
+            
+
+            XmlWriter xmlWriter = XmlWriter.Create(File.OpenWrite("Workflow11.xaml"), writerSettings);
+            XamlXmlWriter xamlWriter = new XamlXmlWriter(xmlWriter, new XamlSchemaContext());
+
+            XamlServices.Save(new test(ActivityXamlServices.CreateBuilderWriter(xamlWriter)), activityBuilder);
+        }
+    }
+}
---
+++ WorkflowConsoleApplication1/test.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xaml;
+
+namespace WorkflowConsoleApplication1
+{
+    class test : XamlWriter
+    {
+        const String c_sapNamespaceURI = "http://schemas.microsoft.com/netfx/2009/xaml/activities/presentation";
+        private int m_attachedPropertyDepth;
+
+        XamlWriter InnerWriter { get; set; }
+        Stack<XamlMember> MemberStack { get; set; }
+        public override XamlSchemaContext SchemaContext
+        {
+            get
+            {
+                return InnerWriter.SchemaContext;
+            }
+        }
+
+        public test(XamlWriter xamlWriter)
+        {
+            this.InnerWriter = xamlWriter;
+            this.MemberStack = new Stack<XamlMember>();
+        }      
+
+        public override void WriteEndMember()
+        {
+            InnerWriter.WriteEndMember();
+        }
+
+        public override void WriteEndObject()
+        {
+            InnerWriter.WriteEndObject();
+        }
+
+        public override void WriteGetObject()
+        {
+            InnerWriter.WriteGetObject();
+        }
+
+        public override void WriteNamespace(NamespaceDeclaration namespaceDeclaration)
+        {
+            InnerWriter.WriteNamespace(namespaceDeclaration);
+        }     
+
+        public override void WriteStartObject(XamlType type)
+        {
+            InnerWriter.WriteStartObject(type);
+        }
+
+        static Boolean IsDesignerAttachedProperty(XamlMember xamlMember)
+        {
+            return xamlMember.IsAttachable &&
+               xamlMember.PreferredXamlNamespace.Equals(c_sapNamespaceURI, StringComparison.OrdinalIgnoreCase);
+        }        
+
+        public override void WriteStartMember(XamlMember xamlMember)
+        {
+            MemberStack.Push(xamlMember);
+            if (IsDesignerAttachedProperty(xamlMember))
+            {
+                m_attachedPropertyDepth++;
+            }
+
+            if (m_attachedPropertyDepth > 0)
+            {
+                return;
+            }
+
+            InnerWriter.WriteStartMember(xamlMember);
+        }
+
+        public override void WriteValue(Object value)
+        {
+            if (m_attachedPropertyDepth > 0)
+            {
+                return;
+            }
+
+            InnerWriter.WriteValue(value);
+        }
+
+
+    }
+}
